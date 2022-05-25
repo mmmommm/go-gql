9a5716eb129a136d6522ab70077389f0974c5cff
Kise Ryota <ryota1839@gmail.com>
2022-05-25 09:51:55 +0900

    implement resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,13 @@
 package graph
 
+import (
+	repo "github.com/mmmommm/go-gql/repository/article"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct {}
+type Resolver struct {
+	Article repo.ArticleRepository
+}
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,18 +5,49 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/google/uuid"
+	domain "github.com/mmmommm/go-gql/domain/article"
 	"github.com/mmmommm/go-gql/graph/generated"
 	"github.com/mmmommm/go-gql/graph/model"
 )
 
+func ProvideGraphHandler() {
+
+}
+
 func (r *mutationResolver) CreateArticle(ctx context.Context, input model.NewArticle) (*model.Article, error) {
-	panic(fmt.Errorf("not implemented"))
+	id := uuid.New().String()
+	err := r.Resolver.Article.Create(ctx, &domain.Article{
+		ID:      id,
+		Title:   input.Title,
+		Content: input.Content,
+	})
+	if err != nil {
+		return nil, err
+	}
+	res := &model.Article{
+		ID:      id,
+		Title:   input.Title,
+		Content: input.Content,
+	}
+	return res, nil
 }
 
 func (r *queryResolver) Articles(ctx context.Context) ([]*model.Article, error) {
-	panic(fmt.Errorf("not implemented"))
+	articles, err := r.Article.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var res []*model.Article
+	for _, article := range articles {
+		res = append(res, &model.Article{
+			ID:      article.ID,
+			Title:   article.Title,
+			Content: article.Content,
+		})
+	}
+	return res, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
